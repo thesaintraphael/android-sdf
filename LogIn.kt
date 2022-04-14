ca52cafc1d5a8a36c7a800dcbe805fa8e46028a7
SamirNasirov <nasirovsamir012@gmail.com>
2022-04-15 01:23:25 +0400

    update login2

--- LogIn.kt
+++ LogIn.kt
@@ -5,46 +5,61 @@ class Users{
 }
 
 class LogIn {
-    var user: Users = Users();
-    var limit: Int = 0;
+    var users: Users = Users();
+    var limit: Int = 1;
 
     fun logIn(){
         while(true){
-            limit += 1;
-            if(limit > 4) {
-                println("The instant interval is exceeded!");
-                break;
-            }
+            this.limit++;
+            if(this.limit > 4)   break;
             
-            print("Username: ");    var username: String = readLine().toString();
-            print("Password: ");    var password: String = readLine().toString();
+            print("Username: ");    var username = readline().toString();     println();
+            print("Password: ");    var password = readline().toString();     println();
             var inputUser = DataUsers(username, password);
 
-            if(validate(inputUser))    println("Good!");
+            if(validate(inputUser))    { println("Good!");	break; }
         }
     }
 
     fun validate(inputUser: DataUsers): Boolean{
+        if (this.limit > 4){
+            println("The instant interval is exceeded!");
+            return false;
+        }
+
         if (inputUser.username.isEmpty()){
             println("Enter username!");
             return false;
         }
-        else if (inputUser.password.isEmpty()){            
+        
+        if (inputUser.password.isEmpty()){            
             println("Enter password!");
             return false;
         }
-        else if (this.user.listOfUsers.find(){it == inputUser} == null){
+        
+        if (this.users.listOfUsers.find(){it == inputUser} == null){
             println("Username or password not corret!");
+            if (this.limit > 1){
+                print("Forgot Password? (Y/n): ");  var answer = "Y"; println("Y");
+                if (answer == "Y"){
+                    Reset(inputUser.username, this.users);
+                    this.limit--;
+                }
+            }
             return false;
         }
-        else{
-            println("Welcome!");
-            return true;
-        }
+        
+        return true;
     }
 }
 
- 
+class Reset(username: String, users: Users){
+    init{
+        var newPassword = readline().toString();
+        users.listOfUsers.find(){ it.username == username }!!.password = newPassword;
+    }
+}
+
 fun main() {
     val test = LogIn();
     test.logIn();
